6f384ef958b2089445ea26848016806a7616f88b
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2025-07-15 16:04:08 +0600

    payment-intent api

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import {MongoClient,ServerApiVersion} from "mongodb";
+import Stripe from "stripe";
 
 const app = express();
 const port = 3000;
 const uri = process.env.MONGO_URI;
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 
 
 // Built-in middlewares
@@ -48,6 +50,36 @@ app.get("/upcoming-meals",async(req,res)=>{
     res.send(result);
 })
 
+// Payment Routes
+app.post("/api/create-payment-intent",async(req,res)=>{
+    const {packageName} = req.body;
+
+    const priceMap = {
+        silver: 999,
+        gold:1999,
+        platinum:2999
+    }
+
+    const amount = priceMap[packageName.toLowerCase()];
+
+    if(!amount){
+        return res.status(400).json({message:"Invalid package name"})
+    }
+
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: "usd",
+            automatic_payment_methods: {enabled: true},
+        })
+
+        res.send({clientSecret:paymentIntent.client_secret});
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({message:"Payment intent failed"});
+    }
+})
 
 
 app.listen(port, () => {
